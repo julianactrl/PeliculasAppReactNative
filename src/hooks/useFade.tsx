3c04ea6b7f6b8f46e3bea81f8cd6096aed05a221
Juliana Gonzalez <juliana.mg3@gmail.com>
2021-10-08 14:26:27 -0300

    Add useFade Hook

---
+++ src/hooks/useFade.tsx
@@ -0,0 +1,22 @@
+import {useRef} from 'react';
+import {Animated} from 'react-native';
+
+export const useFade = () => {
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(); //recibe un callback
+  };
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(); //recibe un callback
+  };
+  return {opacity, fadeIn, fadeOut};
+};
---
+++ src/screens/FadeScreen.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {Animated, Button, View} from 'react-native';
+import {useFade} from '../hooks/useFade';
+
+export const FadeScreen = () => {
+  const {opacity, fadeIn, fadeOut} = useFade();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'gray',
+      }}>
+      <Animated.View
+        style={{
+          backgroundColor: '#084F6A',
+          width: 150,
+          height: 150,
+          borderColor: 'white',
+          borderWidth: 10,
+          opacity: opacity,
+          marginBottom: 10,
+        }}
+      />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="fadeOut" onPress={fadeOut} />
+    </View>
+  );
+};
